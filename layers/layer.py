1ed56817778e002f152b857933642e3b5a12ac48
ztmnbrkt <zt.baracat+github@gmail.com>
2025-01-17 23:39:28 +0000

    corrected base layer, completed dense layer

--- layers/layer.py
+++ layers/layer.py
@@ -2,36 +2,26 @@ from abc import ABC, abstractmethod
 
 class Layer(ABC):
     """
-    Base calss for all network layers.
+    Base class for all network layers.
     """
     def __init__(self):
-        self.input = None
-        self.output = None
+        self.inputs = None
+        self.outputs = None
 
     @abstractmethod
-    def forward(self, input):
+    def forward(self, inputs):
         """
-        Abstract method performing the forward pass for this layer, to be implemented by subclasses.
-
-        Args:
-            input: The input data to the layer
-
-        Returns:
-            output: The data transformed by this layer.
-
+        Abstract method performing the forward propagation for this layer given an input, to be implemented by subclasses.
         """
         ...
 
     @abstractmethod
-    def backward(self, output):
+    def backward(self, output_error, learning_rate):
         """
-        Abstract method performing the backward pass for this layer, to be implemented by subclasses.
-
+        Abstract method performing the backward propagation for this layer, to be implemented by subclasses.
+        
         Args:
-            output: The data to be passed.
-
-        Returns:
-            input: The data transformed by this layer, what would be the "input" data.
-
+            output_error: how "wrong" the network is, used in gradient calcualtions.
+            learning_rate: a scaler for gradient calculations effect.
         """
         ...
--- layers/layer_dense.py
+++ layers/layer_dense.py
@@ -1,4 +1,51 @@
-import layer
+from layer import Layer
+import numpy as np
 
-class LayerDense(layer):
-    def
+class LayerDense(Layer):
+    """
+    The fully connected dense layer, accepts inputs X and returns outputs Y
+    """
+    def __init__(self, input_size, output_size):
+        """
+        Initialises the layer, generating random weight and bias matrices W and B based on input and output sizes.
+        Args:
+            input_size: Number of input neurones
+            output_size: Number of output neurones
+        """
+        super().__init__()
+        self.weights = np.random.rand(input_size, output_size) - 0.5
+        self.biases = np.random.rand(1, output_size) - 0.5
+        
+
+    def forward(self, inputs):
+        """
+        Calculates the dot product of X, W, then adding B.
+        
+        Args:
+            inputs: Data to process
+        
+        Returns:
+            self.outputs: Processed data
+        """
+        self.inputs = inputs
+        self.outputs = np.dot(self.inputs, self.outputs) + self.biases
+        return self.outputs
+    
+    def backward(self, output_error, learning_rate):
+        """
+        Calculates input error dE/dX, weights error dE/dW and  bias error dE/dB for this layers output error dE/dY; 
+        Adjusts W and B accrodingly.
+        
+        Args:
+            output_error: dE/dY, the error of this output
+            learning_rate: A scalar for the effect of this propogation, a
+        Returns:
+            input_error: The calculated input error, dE/dX
+        """
+        input_error = np.dot(output_error, self.weights.T) #T means the matrix's transposition, see docs for more.
+        weights_error = np.dot(self.inputs.T, output_error)
+        # dE/dB = output_error, redundant.
+
+        self.weights -= learning_rate * weights_error
+        self.biases -= learning_rate * output_error
+        return input_error
