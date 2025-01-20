c22a62f1a82f05428fc11d6360610177f0cf20d6
ztmnbrkt <zt.baracat+github@gmail.com>
2025-01-20 10:53:46 +0000

    Completed ReLU

---
+++ activations/relu.py
@@ -0,0 +1,26 @@
+from activation_layer import ActivationLayer
+import numpy as np
+class ReLU(ActivationLayer):
+    """
+    Rectified Linear Unit activation - removes negative values from the input data.
+    """
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self):
+        """
+        Returns the maximum value between 0 and every input element, replacing negative elements with 0,
+        This function allows the network to adapt non-linearly, "rectifying" the linear funciton.
+
+        Args:
+            Inputs: input data (matrix, vector, number)
+
+        Returns:
+            output: the transformed data
+        """
+        self.outputs = np.max(0, self.inputs)
+        return self.outputs
+    
+    def backward(self):
+        raise NotImplementedError
+        ...
